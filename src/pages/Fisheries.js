fcec1a0f021b96139d7602d09e62fc653fdb51ad
‪Yasir Obaid Alshukaili‬‏ <87859841+YasirObaid5@users.noreply.github.com>
2025-06-08 23:51:16 +0400

    Add agriculture website pages with routing

---
+++ src/pages/Fisheries.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Fisheries() {
+  return (
+    <div className="content">
+      <h2>Fisheries</h2>
+      <p>
+        With an extensive coastline along the Arabian Sea and the Gulf of Oman,
+        fisheries are an important sector. Artisanal fishermen land varieties of
+        tuna, sardines and other species. Oman is investing in aquaculture
+        projects to meet growing demand while ensuring sustainability of marine
+        resources.
+      </p>
+    </div>
+  );
+}
